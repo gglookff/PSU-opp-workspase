d8dd4f0d75d18e217210351308dc6201786040e7
are u alive? <164492684+gglookff@users.noreply.github.com>
2025-10-14 20:31:52 +0300

    Add files via upload

---
+++ lab 4/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        
+        string filePath = "spis.txt";
+
+        
+        while (true)
+        {
+            Console.WriteLine("Введите фамилию (или 'стоп' для выхода):");
+            string surname = Console.ReadLine();
+
+            if (surname.ToLower() == "стоп")
+                break;
+
+            Console.WriteLine("Введите имя:");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Введите возраст:");
+            string age = Console.ReadLine();
+
+           
+            StreamWriter writer = new StreamWriter(filePath, true);
+            writer.WriteLine($"{surname} {name} {age} лет");
+            writer.Close();
+
+            Console.WriteLine("Данные записаны!\n");
+        }
+
+        Console.WriteLine("Всё записано в файл spis.txt");
+        Console.ReadKey();
+    }
+}
